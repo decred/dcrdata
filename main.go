2eaed9cac37369a3754fac7c99ba6bfceda9e824
RogueElement <dabiraalawode@yahoo.com>
2017-07-28 17:19:01 +0200

    Block Explorer
    Add files via upload
    
    Update explorer.go
    
    Add files via upload
    
    Added block explorer router as explorer.go
    
    The explorer.go file functions just as the apiroutes.go but for the block explorer pages. It's very similar to apiroutes.go but includes handler functions for the explorer pages
    
    Templates for the explorer and block pages
    
    The basic templates for the explorer and block pages...(I'm bad at styling)
    
    Delete explorer.tmpl
    
    Moved explorer.tmpl to proper folder
    
    Moved block.tmpl to proper folder
    
    Updating  explorer
    
    Templates for the explorer and block pages
    
    The basic templates for the explorer and block pages...(I'm bad at styling)
    
    Moved explorer.tmpl to proper folder
    
    Moved block.tmpl to proper folder
    
    Updating  explorer
    
    Added Transactions Page
    
    Update branch and added block explorer ui
    
    Added a return

--- main.go
+++ main.go
@@ -300,6 +300,7 @@ func mainCore() int {
 	ntfnChans.updateStatusDBHeight <- uint32(sqliteDB.GetHeight())
 
 	apiMux := newAPIRouter(app, cfg.UseRealIP)
+	expMux := newExplorerMux(app, cfg.UseRealIP)
 
 	webMux := chi.NewRouter()
 	webMux.Get("/", webUI.RootPage)
@@ -315,7 +316,7 @@ func mainCore() int {
 		http.Error(w, r.URL.RequestURI()+" ain't no country I've ever heard of! (404)", http.StatusNotFound)
 	})
 	webMux.Mount("/api", apiMux.Mux)
-
+	webMux.Mount("/explorer", expMux.Mux)
 	listenAndServeProto(cfg.APIListen, cfg.APIProto, webMux)
 
 	// Wait for notification handlers to quit
