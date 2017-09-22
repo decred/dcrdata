dac21a9e03f8bcace675adfdcfa000fb50874772
Macaulay Davies <mac114.md@gmail.com>
2017-09-22 23:08:41 +0200

    making addressRows a round effecient number

--- explorer.go
+++ explorer.go
@@ -29,7 +29,7 @@ const (
 	addressTemplateIndex
 	maxExplorerRows = 2000
 	minExplorerRows = 20
-	addressRows     = 700
+	addressRows     = 500
 )
 
 func voutTotal(vouts []dcrjson.Vout) (total float64) {
