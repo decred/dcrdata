9020e50a50a93a8904ba654fddcfc014c100832d
Jon Chappelow <jonathan.chappelow@gmail.com>
2018-01-25 17:04:18 -0800

    stakedb: concurrency fixes
    make ReorgHandler and BlockConnectedHandler Lock() on message
    
    data race for BestNode

--- stakedb/stakedb.go
+++ stakedb/stakedb.go
@@ -156,12 +156,12 @@ func NewStakeDatabase(client *rpcclient.Client, params *chaincfg.Params) (*Stake
 // unlike using db.BestNode.Height(), and checks that the stake database is
 // opened first.
 func (db *StakeDatabase) Height() uint32 {
+	db.nodeMtx.RLock()
+	defer db.nodeMtx.RUnlock()
 	if db == nil || db.BestNode == nil {
 		log.Error("Stake database not yet opened")
 		return 0
 	}
-	db.nodeMtx.RLock()
-	defer db.nodeMtx.RUnlock()
 	return db.BestNode.Height()
 }
 
