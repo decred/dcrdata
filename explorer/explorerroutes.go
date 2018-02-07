ae98f488ce15ec2f6e8f53adefe8bf41714599cc
Macaulay Daives <mac114.md@gmail.com>
2018-02-07 20:30:55 +0100

    bug fixes, adding comments and change to display messsage

--- explorer/explorerroutes.go
+++ explorer/explorerroutes.go
@@ -93,10 +93,11 @@ func (exp *explorerUI) Block(w http.ResponseWriter, r *http.Request) {
 		exp.ErrorPage(w, "Something went wrong...", "could not find that block", true)
 		return
 	}
+	// Checking if there exists any regular non-Coinbase transactions in the block.
 	var count int
 	data.TxAvailable = true
 	for _, i := range data.Tx {
-		if i.Coinbase == true {
+		if i.Coinbase {
 			count++
 		}
 	}
