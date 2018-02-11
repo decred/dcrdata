8dabc1e748ff4e6f9e03850a933a832b12ede9bb
Jonathan Chappelow <chappjc@users.noreply.github.com>
2018-02-11 15:54:45 -0800

    Nil ptr check getexploreraddress (#346)
    * nil pointer check in after GetExplorerAddress
    
    * make updateDevFundBalance yield to other goroutines

--- explorer/explorer.go
+++ explorer/explorer.go
@@ -13,6 +13,7 @@ import (
 	"os/signal"
 	"path/filepath"
 	"reflect"
+	"runtime"
 	"strconv"
 	"strings"
 	"sync"
@@ -505,6 +506,8 @@ func (exp *explorerUI) Store(blockData *blockdata.BlockData, _ *wire.MsgBlock) e
 }
 
 func (exp *explorerUI) updateDevFundBalance() {
+	// yield processor to other goroutines
+	runtime.Gosched()
 	exp.NewBlockDataMtx.Lock()
 	defer exp.NewBlockDataMtx.Unlock()
 
