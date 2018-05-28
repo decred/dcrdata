c1670397c02c2dea3a532500fc9b07127e2bd95f
J Fixby <github@jfixby.com>
2018-05-28 15:37:25 +0200

    replace loggers: /btcsuite/btclog > decred/slog

--- log.go
+++ log.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/btcsuite/btclog"
 	"github.com/decred/dcrd/rpcclient"
 	"github.com/decred/dcrdata/api"
 	"github.com/decred/dcrdata/api/insight"
@@ -22,6 +21,7 @@ import (
 	notify "github.com/decred/dcrdata/notification"
 	"github.com/decred/dcrdata/rpcutils"
 	"github.com/decred/dcrdata/stakedb"
+	"github.com/decred/slog"
 	"github.com/jrick/logrotate/rotator"
 )
 
@@ -47,7 +47,7 @@ var (
 	// backendLog is the logging backend used to create all subsystem loggers.
 	// The backend must not be used before the log rotator has been initialized,
 	// or data races and/or nil pointer dereferences will occur.
-	backendLog = btclog.NewBackend(logWriter{})
+	backendLog = slog.NewBackend(logWriter{})
 
 	// logRotator is one of the logging outputs.  It should be closed on
 	// application shutdown.
@@ -83,7 +83,7 @@ func init() {
 }
 
 // subsystemLoggers maps each subsystem identifier to its associated logger.
-var subsystemLoggers = map[string]btclog.Logger{
+var subsystemLoggers = map[string]slog.Logger{
 	"NTFN": notifyLog,
 	"SQLT": sqliteLog,
 	"PSQL": postgresqlLog,
@@ -127,7 +127,7 @@ func setLogLevel(subsystemID string, logLevel string) {
 	}
 
 	// Defaults to info if the log level is invalid.
-	level, _ := btclog.LevelFromString(logLevel)
+	level, _ := slog.LevelFromString(logLevel)
 	logger.SetLevel(level)
 }
 
