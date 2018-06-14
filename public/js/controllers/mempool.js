cd555589f238a6dce0a8ef7132d84b33b7ec6f24
gozart1 <gozart1@users.noreply.github.com>
2018-06-14 03:24:39 -0700

    dry mempool table row generation

--- public/js/controllers/mempool.js
+++ public/js/controllers/mempool.js
@@ -8,24 +8,41 @@
         }
     }
 
-    function makeNewTable(tx) {
-        var newTable = ""
-        newTable += '<tr><td class="break-word"><span><a class="hash" href="/tx/' +
-        tx.hash + '">' + tx.hash + '</a></span></td>' +
-        '<td class="mono fs15 text-right">' + humanize.decimalParts(tx.total, false, 8, true) + '</td>' +
-        '<td class="mono fs15 text-right">' + tx.size + ' B</td>' +
-        '<td class="mono fs15 text-right" data-target="main.age" data-age="' + tx.time +'">' + humanize.timeSince(tx.time) + '</td></tr>'
-        return newTable
+    function txTableRow(tx) {
+        return `<tr class="flash">
+            <td class="break-word"><span><a class="hash" href="/tx/${tx.hash}" title="${tx.hash}">${tx.hash}</a></span></td>
+            <td class="mono fs15 text-right">${humanize.decimalParts(tx.total, false, 8, true)}</td>
+            <td class="mono fs15 text-right">${tx.size} B</td>
+            <td class="mono fs15 text-right" data-target="main.age" data-age="${tx.time}">${humanize.timeSince(tx.time)}</td>
+        </tr>`
+    }
+
+    function voteTxTableRow(tx) {
+        return `<tr>
+            <td class="break-word"><span><a class="hash" title="(${tx.vote_info.mempool_ticket_index}) ${tx.vote_info.ticket_spent}" href="/tx/${tx.hash}">${tx.hash}</a></span></td>
+            <td class="mono fs15">${tx.vote_info.vote_version}</td>
+            <td class="mono fs15">${tx.vote_info.block_validation.validity}</td>
+            <td class="mono fs15">${tx.vote_info.vote_choices[0].id}</td>
+            <td class="mono fs15">${tx.vote_info.vote_choices[0].choice.Id}</td>
+            <td class="mono fs15 text-right">${humanize.decimalParts(tx.total, false, 8, true)}</td>
+            <td class="mono fs15">${tx.size} B</td>
+            <td class="mono fs15 text-right" data-age="${tx.time}">${humanize.timeSince(tx.time)}</td>
+        </tr>`
+    }
+
+    function txFlexTableRow(tx) {
+        return `<div class="d-flex flex-table-row flash">
+            <a class="hash truncate-hash" style="flex: 1 1 auto" href="/tx/${tx.hash}" title="${tx.hash}">${tx.hash}</a>
+            <span style="flex: 0 0 60px" class="mono text-right ml-1">${tx.Type}</span>
+            <span style="flex: 0 0 105px" class="mono text-right ml-1">${humanize.decimalParts(tx.total, false, 8, true)}</span>
+            <span style="flex: 0 0 50px" class="mono text-right ml-1">${tx.size} B</span>
+            <span style="flex: 0 0 65px" class="mono text-right ml-1" data-target="main.age" data-age="${tx.time}">${humanize.timeSince(tx.time)}</span>
+        </div>`
     }
 
     function addTxRow(tx, target) {
         var rows = $(target).find('tr')
-        var newRow = '<tr class="flash"><td class="break-word"><span><a class="hash" href="/tx/' +
-            tx.hash + '">' + tx.hash + '</a></span></td>' +
-            '<td class="mono fs15 text-right"> ' + humanize.decimalParts(tx.total, false, 8, true) +'</td>' +
-            '<td class="mono fs15 text-right">' + tx.size + ' B</td>' +
-            '<td class="mono fs15 text-right" data-age="' + tx.time +'">' + humanize.timeSince(tx.time) +'</td>'
-        var newRowHtml = $.parseHTML(newRow)
+        var newRowHtml = $.parseHTML(txTableRow(tx))
         $(newRowHtml).insertBefore(rows.first())
     }
 
@@ -72,13 +89,7 @@
                 }
                 var rows = $(this.transactionsTarget).find(".flex-table-row")
                 rows.last().remove()
-                $($.parseHTML(`<div class="d-flex flex-table-row flash">
-                    <a class="hash truncate-hash" style="flex: 1 1 auto" href="/tx/${tx.hash}" title="${tx.hash}">${tx.hash}</a>
-                    <span style="flex: 0 0 60px" class="mono text-right ml-1">${tx.Type}</span>
-                    <span style="flex: 0 0 105px" class="mono text-right ml-1">${humanize.decimalParts(tx.total, false, 8, true)}</span>
-                    <span style="flex: 0 0 50px" class="mono text-right ml-1">${tx.size} B</span>
-                    <span style="flex: 0 0 65px" class="mono text-right ml-1" data-target="main.age" data-age="${tx.time}">${humanize.timeSince(tx.time)}</span>
-                </div>`)).insertBefore(rows.first())
+                $($.parseHTML(txFlexTableRow(tx))).insertBefore(rows.first())
             })
         }
     })
@@ -137,32 +148,20 @@
         handleTxsResp(event) {
             var m = JSON.parse(event)
 
-            var ticketsTable = _.map(m.tickets, makeNewTable).join("")
+            var ticketsTable = _.map(m.tickets, txTableRow).join("")
             $(this.ticketTransactionsTarget).html($.parseHTML(ticketsTable))
 
-            var transactionsTable = _.map(m.tx, makeNewTable).join("")
+            var transactionsTable = _.map(m.tx, txTableRow).join("")
             $(this.regularTransactionsTarget).html($.parseHTML(transactionsTable))
 
             // TODO handle no revokes scenario properly
-            if (m.num_revokes > 0) {
-                var revokesTable = _.map(m.revokes, makeNewTable).join("")
+            if (m.num_revokes > 0 && this.revokeTransactionsTarget) {
+                var revokesTable = _.map(m.revokes, txTableRow).join("")
                 $(this.revokeTransactionsTarget).html($.parseHTML(revokesTable))
             }
 
-            var votesTable = ''
-            _.each(m.votes, (tx) => {
-                var rows = $(this.voteTransactionsTarget).find('tr')
-                votesTable += '<tr><td class="break-word"><span><a class="hash" title="(' + tx.vote_info.mempool_ticket_index +
-                    ') ' + tx.vote_info.ticket_spent + '" href="/tx/' + tx.hash + '">' + tx.hash + '</a></span></td>' +
-                    '<td class="mono fs15">' + tx.vote_info.vote_version + '</td>' +
-                    '<td class="mono fs15">' + tx.vote_info.block_validation.validity + '</td>' +
-                    '<td class="mono fs15">' + tx.vote_info.vote_choices[0].id + '</td>' +
-                    '<td class="mono fs15">' + tx.vote_info.vote_choices[0].choice.Id + '</td>' +
-                    '<td class="mono fs15 text-right"> ' + humanize.decimalParts(tx.total, false, 8, true) + '</td>' +
-                    '<td class="mono fs15">' + tx.size + ' B</td>'  +
-                    '<td class="mono fs15 text-right" data-age="' + tx.time +'">' + humanize.timeSince(tx.time) + '</td>'
-            })
-            $(this.voteTranscationsTarget).html($.parseHTML(votesTable))
+            var votesTable = _.map(m.votes, voteTxTableRow).join("")
+            $(this.voteTransactionsTarget).html($.parseHTML(votesTable))
         }
 
         renderNewTxns(evt) {
@@ -172,15 +171,7 @@
                 switch (tx.Type) {
                     case "Vote":
                         var rows = $(this[tx.Type.toLowerCase() + "TransactionsTarget"]).find('tr')
-                        var newRow = '<tr><td class="break-word"><span><a class="hash" title="(' + tx.vote_info.mempool_ticket_index +
-                            ') ' + tx.vote_info.ticket_spent + '" href="/tx/' + tx.hash + '">' + tx.hash + '</a></span></td>' +
-                            '<td class="mono fs15">' + tx.vote_info.vote_version + '</td>' +
-                            '<td class="mono fs15">' + tx.vote_info.block_validation.validity + '</td>' +
-                            '<td class="mono fs15">' + tx.vote_info.vote_choices[0].id + '</td>' +
-                            '<td class="mono fs15">' + tx.vote_info.vote_choices[0].choice.Id + '</td>' +
-                            '<td class="mono fs15 text-right"> ' + humanize.decimalParts(tx.total, false, 8, true) + '</td>' +
-                            '<td class="mono fs15">' + tx.size + ' B</td>' +
-                            '<td class="mono fs15 text-right" data-age="' + tx.time +'">' + humanize.timeSince(tx.time) + '</td>'
+                        var newRow = voteTxTableRow(tx)
                         var newRowHtml = $.parseHTML(newRow)
                         $(newRowHtml).insertBefore(rows.first())
                         break
