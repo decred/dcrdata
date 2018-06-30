85706908ace79ef19af5c0b5d7df52cddd330ec8
Jonathan Chappelow <chappjc@users.noreply.github.com>
2018-06-30 09:28:34 -0700

    testutil functions take *testing.T (#1)
    cleanup

--- db/dcrsqlite/dcrsqlite_test.go
+++ db/dcrsqlite/dcrsqlite_test.go
@@ -9,11 +9,12 @@ import (
 // TestMissingParentFolder ensures InitDB() is able to create a new DB-file parent directory if necessary
 // See https://github.com/decred/dcrdata/issues/515
 func TestMissingParentFolder(t *testing.T) {
-	testutil.ResetTempFolder()
-	targetDBFile := testutil.FilePathInsideTempDir("x/y/z/" + testutil.DefaultDBFileName)
+	// Specify DB file in non-existent path
+	testutil.ResetTempFolder(t)
+	targetDBFile := testutil.FilePathInsideTempDir(t, "x/y/z/"+testutil.DefaultDBFileName)
+
 	dbInfo := &DBInfo{FileName: targetDBFile}
 	db, err := InitDB(dbInfo)
-
 	if err != nil {
 		t.Fatalf("InitDB() failed: %v", err)
 	}
--- testutil/tempfiles.go
+++ testutil/tempfiles.go
@@ -1,8 +1,10 @@
+// package testutil provides some helper functions to be used in unit tests.
 package testutil
 
 import (
 	"os"
 	"path/filepath"
+	"testing"
 )
 
 const (
@@ -10,33 +12,34 @@ const (
 	DefaultDBFileName  = "test.dcrdata.sqlt.db"
 )
 
-// TempFolderPath returns path of a temporary directory used by these tests to store data
-func TempFolderPath() string {
+// TempFolderPath returns path of a temporary directory used by these tests to
+// store data.
+func TempFolderPath(t *testing.T) string {
 	testDir, err := filepath.Abs(DefaultDataDirname)
 	if err != nil {
-		panic("Failed to produce DB-test folder path")
+		t.Fatalf("Failed to produce DB-test folder path")
 	}
 	return testDir
 }
 
-// Ensures we run our test in a clean room. Removes all files created by any of these tests in the temp directory.
-func ResetTempFolder() {
-	testFolderPath := TempFolderPath()
+// Ensures we run our test in a clean room. Removes all files created by any of
+// these tests in the temp directory.
+func ResetTempFolder(t *testing.T) {
+	testFolderPath := TempFolderPath(t)
 	err := os.RemoveAll(testFolderPath)
-	//Failed to clear test-files
+	// Failed to clear test-files
 	if err != nil {
-		panic("Failed to clear temp folder")
+		t.Fatalf("Failed to clear temp folder")
 	}
 }
 
-// FilePathInsideTempDir creates a path to a file inside the temp directory
-func FilePathInsideTempDir(pathInsideTempFolder string) string {
-	tempDir := TempFolderPath()
+// FilePathInsideTempDir creates a path to a file inside the temp directory.
+func FilePathInsideTempDir(t *testing.T, pathInsideTempFolder string) string {
+	tempDir := TempFolderPath(t)
 	targetPath := filepath.Join(tempDir, pathInsideTempFolder)
 	targetPath, err := filepath.Abs(targetPath)
-
 	if err != nil {
-		panic("Failed to build a path: " + pathInsideTempFolder)
+		t.Fatalf("Failed to build a path: " + pathInsideTempFolder)
 	}
 	return targetPath
 }
