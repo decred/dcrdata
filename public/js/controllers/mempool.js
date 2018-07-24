f8af616b85c8517884bd91c741a8164b9f917e7f
mikefullman <mikef@zenshows.com>
2018-07-24 13:14:42 -0700

    gozart review:  camelCase, moved functions inside controller class, trailing whitespace,removed unused IDs, rows.html, updated classes

--- public/js/controllers/mempool.js
+++ public/js/controllers/mempool.js
@@ -8,12 +8,6 @@
         }
     }
 
-    function emptyRow(txnType, colspan) {
-        return `<tr>
-            <td colspan="${colspan}">No ${txnType} in mempool</td>
-        </tr>`
-    }
-
     function txTableRow(tx) {
         return `<tr class="flash">
             <td class="break-word"><span><a class="hash" href="/tx/${tx.hash}" title="${tx.hash}">${tx.hash}</a></span></td>
@@ -59,47 +53,6 @@
         var rows = $(target).find('tr')
         var newRowHtml = $.parseHTML(rowFn(tx))
         $(newRowHtml).insertBefore(rows.first())
-    }
-
-    function filterVotesLastBlock(target) {
-        var best_block_hash=$(target).data("hash");
-        var best_block_height=$(target).text();
-        console.log(best_block_height);
-        $('*[data-target="mempool.voteTransactions"] tr').each(function(i,el) {
-            var vote_validation_hash=$(this).data("blockhash");
-            var vote_block_height=$(this).data("height");
-            if(vote_block_height>best_block_height) {
-                $(this).closest("tr").addClass("upcoming-vote");
-                $(this).closest("tr").removeClass("old-vote");
-            }else if(vote_validation_hash!=best_block_hash){
-                $(this).closest("tr").addClass("old-vote");
-                $(this).closest("tr").removeClass("upcoming-vote");
-                $(this).find("td.last_block").text("Invalid");
-            }else{
-                $(this).closest("tr").removeClass("old-vote");
-                $(this).closest("tr").removeClass("upcoming-vote");
-                $(this).find("td.last_block").text("Valid");  
-            }
-        })
-    };
-
-    function sortVotesTable() {
-        var $table = $('*[data-target="mempool.voteTransactions"] tr').parent();
-        var $rows =$('*[data-target="mempool.voteTransactions"] tr');
-        $rows.sort(function(a, b){
-            var heightA = parseInt($('td:nth-child(2)',a).text());
-            var heightB = parseInt($('td:nth-child(2)',b).text());
-            if(heightA == heightB) {
-                 var indexA = parseInt($('td:nth-child(3)',a).text());
-                 var indexB = parseInt($('td:nth-child(3)',b).text());
-                 return (indexA - indexB);
-            }else{
-                return (heightB - heightA);
-            }
-        });
-        $.each($rows, function(index, row){
-          $table.append(row);
-        });
     }
 
     app.register("homepageMempool", class extends Stimulus.Controller {
@@ -150,7 +103,6 @@
         }
     })
 
-
     app.register("mempool", class extends Stimulus.Controller {
         static get targets() {
             return [
@@ -174,8 +126,8 @@
         connect() {
             ws.registerEvtHandler("newtx", (evt) => {
                 this.renderNewTxns(evt)
-                filterVotesLastBlock(this.bestBlockTarget);
-                sortVotesTable();
+                this.filterVotesLastBlock();
+                this.sortVotesTable();
                 keyNav(evt, false, true)
             })
             ws.registerEvtHandler("mempool", (evt) => {
@@ -184,10 +136,10 @@
             });
             ws.registerEvtHandler("getmempooltxsResp", (evt) => {
                 this.handleTxsResp(evt)
-                filterVotesLastBlock(this.bestBlockTarget);
-                sortVotesTable();
+                this.filterVotesLastBlock();
+                this.sortVotesTable();
                 keyNav(evt, false, true)
-            })    
+            })
         }
 
         disconnect() {
@@ -213,7 +165,7 @@
             $(this.totalNeededTarget).text(m.voting_info.total_votes_required)
             $(this.totalOutTarget).html(`${humanize.decimalParts(m.total, false, 8, true)}`);
             $(this.mempoolSizeTarget).text(m.formatted_size);
-            filterVotesLastBlock(this.bestBlockTarget);
+            this.filterVotesLastBlock();
         }
 
         handleTxsResp(event) {
@@ -231,6 +183,43 @@
                 var rowFn = tx.Type === "Vote" ? voteTxTableRow : txTableRow
                 addTxRow(tx, this[tx.Type.toLowerCase() + "TransactionsTarget"], rowFn)
             })
+        }
+
+        filterVotesLastBlock() {
+            var bestBlockHash=$(this.bestBlockTarget).data("hash");
+            var bestBlockHeight=$(this.bestBlockTarget).text();
+            $(this.voteTransactionsTarget).children("tr").each(function(i,el) {
+                var voteValidationHash=$(this).data("blockhash");
+                var voteBlockHeight=$(this).data("height");
+                if(voteBlockHeight>bestBlockHeight) {
+                    $(this).closest("tr").addClass("upcoming-vote");
+                    $(this).closest("tr").removeClass("old-vote");
+                }else if(voteValidationHash!=bestBlockHash){
+                    $(this).closest("tr").addClass("old-vote");
+                    $(this).closest("tr").removeClass("upcoming-vote");
+                    $(this).find("td.last_block").text("Invalid");
+                }else{
+                    $(this).closest("tr").removeClass("old-vote");
+                    $(this).closest("tr").removeClass("upcoming-vote");
+                    $(this).find("td.last_block").text("Valid");
+                }
+            })
+        };
+
+        sortVotesTable() {
+            var $rows = $(this.voteTransactionsTarget).children("tr");
+            $rows.sort(function(a, b){
+                var heightA = parseInt($('td:nth-child(2)',a).text());
+                var heightB = parseInt($('td:nth-child(2)',b).text());
+                if(heightA == heightB) {
+                     var indexA = parseInt($('td:nth-child(3)',a).text());
+                     var indexB = parseInt($('td:nth-child(3)',b).text());
+                     return (indexA - indexB);
+                }else{
+                    return (heightB - heightA);
+                }
+            });
+            $(this.voteTransactionsTarget).html($rows);
         }
     })
 })()
