0126f8b3112284da6f69a7c81a6c834c0a397a18
J Fixby <github@jfixby.com>
2018-08-07 00:51:29 +0200

    dcrsqlite: test GetBestBlockHash, GetBestBlockHeight, GetStakeInfoHeight

--- db/dcrsqlite/blocksretrieval_test.go
+++ db/dcrsqlite/blocksretrieval_test.go
@@ -48,3 +48,41 @@ func TestEmptyDBRetrieveBlockFeeInfo(t *testing.T) {
 			cmp.Diff(*result, defaultChartsData))
 	}
 }
+
+func TestEmptyDBGetBestBlockHash(t *testing.T) {
+	testutil.BindCurrentTestSetup(t)
+	db := ObtainReusableEmptyDB()
+	str := db.GetBestBlockHash()
+	if str != "" {
+		testutil.ReportTestFailed(
+			"GetBestBlockHash() failed: expected empty string, returned %v",
+			str)
+	}
+}
+
+func TestEmptyDBGetBestBlockHeight(t *testing.T) {
+	testutil.BindCurrentTestSetup(t)
+	db := ObtainReusableEmptyDB()
+	h := db.GetBestBlockHeight()
+	if h != -1 {
+		testutil.ReportTestFailed(
+			"db.GetBestBlockHeight() returned %d, expected -1",
+			h)
+	}
+}
+
+func TestEmptyDBGetStakeInfoHeight(t *testing.T) {
+	testutil.BindCurrentTestSetup(t)
+	db := ObtainReusableEmptyDB()
+	endHeight, err := db.GetStakeInfoHeight()
+	if err != nil {
+		testutil.ReportTestFailed(
+			"GetStakeInfoHeight() failed: %v",
+			err)
+	}
+	if endHeight != -1 {
+		testutil.ReportTestFailed(
+			"GetStakeInfoHeight() failed: returned %d, expected -1",
+			endHeight)
+	}
+}
