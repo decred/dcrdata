5fdea33a68b536f6361f53b717afedc9abf68cd8
Jonathan Chappelow <chappjc@protonmail.com>
2018-08-14 10:18:15 -0500

    derps

--- main.go
+++ main.go
@@ -354,7 +354,7 @@ func mainCore() error {
 	// After sync and indexing, must use upsert statement, which checks for
 	// duplicate entries and updates instead of erroring. SyncChainDB should set
 	// this on successful sync, but do it again anyway.
-	db.EnableDuplicateCheckOnInsert(true)
+	auxDB.EnableDuplicateCheckOnInsert(true)
 
 	// The sync routines may have lengthy tasks, such as table indexing, that
 	// follow main sync loop. Before enabling the chain monitors, ensure the DBs
