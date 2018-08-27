e971d94a5c3c592795e0d6fd73e1f8d5521c2417
dmigwi <migwi.ndungu@andela.com>
2018-08-27 19:58:10 +0300

    Add merge ticket pool data function

--- db/dbtypes/conversion.go
+++ db/dbtypes/conversion.go
@@ -3,6 +3,8 @@ package dbtypes
 import (
 	"fmt"
 
+	"github.com/decred/dcrd/dcrutil"
+
 	"github.com/decred/dcrd/chaincfg"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrdata/txhelpers"
@@ -81,5 +83,57 @@ func ChartGroupingToInterval(grouping ChartGrouping) (float64, error) {
 
 	default:
 		return -1, fmt.Errorf(`unknown chart grouping "%d"`, grouping)
+	}
+}
+
+// MergeTicketPoolPurchases merges the immature and live tickets distribution data
+// needed to plot the tickets purchase distribution real time graph. Since
+// most of the data in the arrays is ordered only the last 10 entries for the
+// immature tickets data and first 10 live tickets data are used to merge the
+// overlapping entries.
+func MergeTicketPoolPurchases(immatureTickets PoolTicketsData, liveTickets PoolTicketsData) PoolTicketsData {
+	var track = make(map[uint64]int)
+	// defines the length from where immature ticket data sorting should start from.
+	var count = len(immatureTickets.Time) - 10
+
+	var unsortedTime = append(immatureTickets.Time[count:], liveTickets.Time[0:10]...)
+	var unsortedImmature = append(immatureTickets.Immature[count:], liveTickets.Immature[0:10]...)
+	var unsortedLive = append(immatureTickets.Live[count:], liveTickets.Live[0:10]...)
+	var unsortedRawPrice = append(immatureTickets.RawPrice[count:], liveTickets.RawPrice[0:10]...)
+
+	var sortedPrice []float64
+	var sortedImmature, sortedLive, sortedTime, sortedRawPrice []uint64
+
+	for i, foundTime := range unsortedTime {
+		if val, ok := track[foundTime]; ok {
+			totalRawPrice := unsortedRawPrice[i] + unsortedRawPrice[val]
+
+			sortedLive[val] = unsortedLive[i]
+			sortedPrice[val] = dcrutil.Amount(totalRawPrice /
+				(unsortedLive[i] + unsortedImmature[val])).ToCoin()
+		} else {
+			track[foundTime] = len(sortedTime)
+			sortedTime = append(sortedTime, foundTime)
+			sortedImmature = append(sortedImmature, unsortedImmature[i])
+			sortedLive = append(sortedLive, unsortedLive[i])
+			sortedPrice = append(sortedPrice, dcrutil.Amount(unsortedRawPrice[i]/
+				(unsortedLive[i]+unsortedImmature[i])).ToCoin())
+			sortedRawPrice = append(sortedRawPrice, unsortedRawPrice[i])
+		}
+	}
+
+	// Append the immature tickets sorted data at the start of the respective arrays.
+	sortedTime = append(immatureTickets.Time[:count], sortedTime...)
+	sortedImmature = append(immatureTickets.Immature[:count], sortedImmature...)
+	sortedLive = append(immatureTickets.Live[:count], sortedLive...)
+	sortedPrice = append(immatureTickets.Price[:count], sortedPrice...)
+
+	// Append the live tickets sorted data at the end of the respective arrays and
+	// return the sorted merged tickets purchase distribution data.
+	return PoolTicketsData{
+		Time:     append(sortedTime, liveTickets.Time[10:]...),
+		Immature: append(sortedImmature, liveTickets.Immature[10:]...),
+		Live:     append(sortedLive, liveTickets.Live[10:]...),
+		Price:    append(sortedPrice, liveTickets.Price[10:]...),
 	}
 }
