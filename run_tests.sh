3beacda981a8e59e03ec161f37893241f02ac191
Jonathan Chappelow <chappjc@users.noreply.github.com>
2018-09-08 09:15:16 -0700

    limit CI to go1.11 and fix some tests (#673)
    tests/CI: Test only Go 1.11 and disable linters.
    
    This limits Travis to testing Go 1.11 only, and uses go modules exclusively, no dep.
    
    GO111MODULE=on is set, and the vendor folder is not populated with dep or go mod vendor. As such, linters are disabled until such time that the linter authors can figure out how to make them work with modules.
    
    This also fixes some testing issues:
     - stakedb (ticketpool) test data in wrong folder.
     - Missing formatting directives in testutil functions.

--- run_tests.sh
+++ run_tests.sh
@@ -13,34 +13,16 @@ set -ex
 # gometalinter (github.com/alecthomas/gometalinter) is used to run each each
 # static checker.
 
-GOVERSION=${1:-1.10}
+GOVERSION=${1:-1.11}
 REPO=dcrdata
 DOCKER_IMAGE_TAG=decred-golang-builder-$GOVERSION
 
 testrepo () {
   TMPFILE=$(mktemp)
-
-  # Update /vendor, but not Gopkg.lock
-  dep ensure -vendor-only
-
-  # Check linters
-  gometalinter --vendor --disable-all --deadline=10m \
-    --enable=vet \
-    --enable=gosimple \
-    --enable=unconvert \
-    --enable=ineffassign \
-    ./...
-  if [ $? != 0 ]; then
-    echo 'gometalinter has some complaints'
-    exit 1
-  fi
+  export GO111MODULE=on
 
   # Test application install
-  if [ $GOVERSION == 1.10 ]; then
-    go install -i . ./cmd/...
-  else
-    go install . ./cmd/...
-  fi
+  go install . ./cmd/...
   if [ $? != 0 ]; then
     echo 'go install failed'
     exit 1
@@ -48,7 +30,7 @@ testrepo () {
 
   # Check tests
   git clone https://github.com/dcrlabs/bug-free-happiness test-data-repo
-  tar xvf test-data-repo/stakedb/test_ticket_pool.bdgr.tar.xz
+  tar xvf test-data-repo/stakedb/test_ticket_pool.bdgr.tar.xz -C ./stakedb
 
   env GORACE='halt_on_error=1' go test -v -race ./...
   if [ $? != 0 ]; then
