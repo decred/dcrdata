e174cbf3adbf24a1bfcbcc3f5ca65d753f0714f0
Brian <buck54321@gmail.com>
2018-11-01 11:00:14 -0500

    now with CommonPageData docs

--- explorer/explorertypes.go
+++ explorer/explorertypes.go
@@ -593,6 +593,9 @@ type StatsInfo struct {
 	RewardWindowSize           int64
 }
 
+// CommonPageData is the basis for dasta structs used for HTML templates.
+// explorerUI.commonData returns an initialized instance or CommonPageData,
+// which itself should be used to initialize page data template structs.
 type CommonPageData struct {
 	Tip           *WebBasicBlock
 	Version       string
