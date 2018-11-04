c3601204fd7babd904d1fc72df23d628de35d2e8
Ademu Anthony <ademuanthony@gmail.com>
2018-11-04 14:58:22 +0100

    removed whitespace in js

--- public/js/controllers/extras.js
+++ public/js/controllers/extras.js
@@ -1,24 +1,19 @@
-
 (() => {
-
     app.register("extras", class extends Stimulus.Controller {
         static get targets() {
             return [ "confirmations" ]
         }
-
         initialize() {
             let that = this
             globalEventBus.on("BLOCK_RECEIVED", function (newBlock) {
                 that.refreshConfirmations(newBlock.block.height)
             })
         }
-
         connect() {
             this.confirmationsTargets.forEach((el,i) => {
                 this.setConfirmationText(el, el.dataset.confirmations)
             })
         }
-
         setConfirmationText(el, confirmations) {
             if(!el.dataset.formatted){
                 el.textContent = confirmations
@@ -30,12 +25,10 @@
                 el.textContent = "(unconfirmed)"
             }
         }
-
         refreshConfirmations(newHeight) {
             this.confirmationsTargets.forEach((el,i) => {
                 let blockHeight = el.dataset.confirmationBlockHeight
                 let confirmations = newHeight - blockHeight
-
                 this.setConfirmationText(el, confirmations)
                 el.dataset.confirmations = confirmations
                 el.dataset.confirmationBlockHeight = blockHeight
--- public/js/eventbus.js
+++ public/js/eventbus.js
@@ -3,14 +3,11 @@ function EventBus() {
 
     this.on = function( eventType, callback ) {
         const eventCallbacksPair = findEventCallbacksPair(eventType)
-
         if(eventCallbacksPair)
             eventCallbacksPair.callbacks.push(callback)
         else
             eventCallbacksPairs.push( new EventCallbacksPair(eventType, callback) )
     }
-
-
     this.publish = function( eventType, args ) {
         const eventCallbacksPair = findEventCallbacksPair(eventType)
 
@@ -21,11 +18,9 @@ function EventBus() {
 
         eventCallbacksPair.callbacks.forEach( callback => callback(args) )
     }
-
     function findEventCallbacksPair(eventType) {
         return eventCallbacksPairs.find( eventObject => eventObject.eventType === eventType )
     }
-
     function EventCallbacksPair( eventType, callback ) {
         this.eventType = eventType
         this.callbacks = [callback]
