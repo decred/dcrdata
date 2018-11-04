a5d436562e13baf086eef4da989cf134f2677092
Ademu Anthony <ademuanthony@gmail.com>
2018-11-04 14:05:52 +0100

    added a pub/sub eventbus

---
+++ public/js/eventbus.js
@@ -0,0 +1,35 @@
+function EventBus() {
+    const eventCallbacksPairs = []
+
+    this.on = function( eventType, callback ) {
+        const eventCallbacksPair = findEventCallbacksPair(eventType)
+
+        if(eventCallbacksPair)
+            eventCallbacksPair.callbacks.push(callback)
+        else
+            eventCallbacksPairs.push( new EventCallbacksPair(eventType, callback) )
+    }
+
+
+    this.publish = function( eventType, args ) {
+        const eventCallbacksPair = findEventCallbacksPair(eventType)
+
+        if(!eventCallbacksPair) {
+            console.error("no subscribers for event " +eventType)
+            return;
+        }
+
+        eventCallbacksPair.callbacks.forEach( callback => callback(args) )
+    }
+
+    function findEventCallbacksPair(eventType) {
+        return eventCallbacksPairs.find( eventObject => eventObject.eventType === eventType )
+    }
+
+    function EventCallbacksPair( eventType, callback ) {
+        this.eventType = eventType
+        this.callbacks = [callback]
+    }
+}
+
+var globalEventBus = new EventBus()
