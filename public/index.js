df3a47b274cc78b5b1636cb0c4eb30c7706d0525
Brian <buck54321@gmail.com>
2018-12-26 17:33:02 -0600

    Remove intermediate block variable.

--- public/index.js
+++ public/index.js
@@ -46,8 +46,7 @@ async function createWebSocket (loc) {
   var updateBlockData = function (event) {
     console.log('Received newblock message', event)
     var newBlock = JSON.parse(event)
-    var b = newBlock.block
-    b.unixStamp = (new Date(b.time)).getTime() / 1000
+    newBlock.block.unixStamp = (new Date(newBlock.block.time)).getTime() / 1000
     globalEventBus.publish('BLOCK_RECEIVED', newBlock)
   }
   ws.registerEvtHandler('newblock', updateBlockData)
