c3d6b44f812458927ccda371a7962c10ee2b8d93
Jonathan Chappelow <jonathan.chappelow@gmail.com>
2018-12-26 17:14:06 -0600

    CSV download link specifies line endings
    Instead of the API's CSV download handler inspecting the user agent,
    use a URL query parameter (cr=true) to request a CSV file with CRLF line
    endings.
    The address page handler inspects the user agent and generates the
    appropriate API endpoint link.
    
    copyright year

--- explorer/explorerroutes.go
+++ explorer/explorerroutes.go
@@ -1203,10 +1203,11 @@ func (exp *explorerUI) AddressPage(w http.ResponseWriter, r *http.Request) {
 	// AddressPageData is the data structure passed to the HTML template
 	type AddressPageData struct {
 		*CommonPageData
-		Data       *dbtypes.AddressInfo
-		NetName    string
-		IsLiteMode bool
-		ChartData  *dbtypes.ChartsData
+		Data         *dbtypes.AddressInfo
+		NetName      string
+		IsLiteMode   bool
+		ChartData    *dbtypes.ChartsData
+		CRLFDownload bool
 	}
 
 	// Grab the URL query parameters
@@ -1285,12 +1286,17 @@ func (exp *explorerUI) AddressPage(w http.ResponseWriter, r *http.Request) {
 	addrData.IsDummyAddress = isZeroAddress // may be redundant
 	addrData.Path = r.URL.Path
 
+	// For Windows clients only, link to downloads with CRLF (\r\n) line
+	// endings.
+	UseCRLF := strings.Contains(r.UserAgent(), "Windows")
+
 	// Execute the HTML template.
 	pageData := AddressPageData{
 		CommonPageData: exp.commonData(),
 		Data:           addrData,
 		IsLiteMode:     exp.liteMode,
 		NetName:        exp.NetName,
+		CRLFDownload:   UseCRLF,
 	}
 	str, err := exp.templates.execTemplateToString("address", pageData)
 	if err != nil {
