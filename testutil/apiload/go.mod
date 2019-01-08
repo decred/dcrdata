529527bac75d8bf0b3f0b784a329ab50abca0109
Brian <buck54321@gmail.com>
2019-01-08 07:08:46 -0600

    load testing

---
+++ testutil/apiload/go.mod
@@ -0,0 +1,12 @@
+module github.com/decred/dcrdata/testutil/apiload
+
+require (
+	github.com/btcsuite/go-flags v0.0.0-20150116065318-6c288d648c1c
+	github.com/c2h5oh/datasize v0.0.0-20171227191756-4eba002a5eae // indirect
+	github.com/influxdata/tdigest v0.0.0-20181121200506-bf2b5ad3c0a9 // indirect
+	github.com/mailru/easyjson v0.0.0-20180823135443-60711f1a8329 // indirect
+	github.com/tsenart/go-tsz v0.0.0-20180814235614-0bd30b3df1c3 // indirect
+	github.com/tsenart/vegeta v12.1.0+incompatible
+	golang.org/x/net v0.0.0-20181220203305-927f97764cc3 // indirect
+	golang.org/x/text v0.3.0 // indirect
+)
---
+++ testutil/apiload/main.go
@@ -0,0 +1,257 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	vegeta "github.com/tsenart/vegeta/lib"
+)
+
+const (
+	OutputFileTemplate = "result_%s_%s.json"
+)
+
+// Attack specifies a duration and a series of attackers, each with their own
+// frequency and targets.
+type Attack struct {
+	Duration  int         `json:"duration"`
+	Attackers []*Attacker `json:"attackers"`
+}
+
+// Frequency is 1/second and endpoints are relative to root
+type Attacker struct {
+	Name      string   `json:"name"`
+	Frequency int      `json:"frequency"`
+	Delay     float32  `json:"delay"`
+	Endpoints []string `json:"endpoints"`
+}
+
+var Config *config
+
+func main() {
+	var err error
+
+	Config, err = loadConfig()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	// Profiles load from a JSON file. The default profiles are in profiles.json.
+	profiles, err := loadProfiles()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	if Config.ListAttacks {
+		listAttacks(profiles)
+		return
+	}
+
+	if Config.Attack == "" {
+		fmt.Println("No attack specified. Specify an attack with -a.\n")
+		listAttacks(profiles)
+		return
+	}
+
+	attackProfile, ok := profiles[Config.Attack]
+	if !ok {
+		fmt.Printf("Specified attack profile, %s, not found in definitions at %s\n\n", Config.Attack, Config.ProfilesPath)
+		listAttacks(profiles)
+		return
+	}
+
+	err = createResultsDirectory()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	// If CPUs are specified, set a max
+	if Config.CPUs >= 1 {
+		runtime.GOMAXPROCS(Config.CPUs)
+		log.Printf("Running on %d CPUs.", Config.CPUs)
+	}
+
+	var wg sync.WaitGroup
+	resChan := make(chan *vegeta.Result)
+
+	// Duration from profile can be overridden in config or cli argument.
+	seconds := attackProfile.Duration
+	if Config.Duration > 0 {
+		seconds = Config.Duration
+	}
+
+	// Perform the load test.
+	log.Printf("Beginning %s. Duration: %d seconds.", Config.Attack, seconds)
+	duration := time.Duration(seconds*1000) * time.Millisecond
+	for idx, attacker := range attackProfile.Attackers {
+		if len(attacker.Endpoints) == 0 {
+			log.Fatalf("Empty enpoint list encountered for %s", Config.Attack)
+		}
+		rate := vegeta.Rate{
+			Freq: attacker.Frequency,
+			Per:  time.Second,
+		}
+
+		var targets []vegeta.Target
+		for _, endpoint := range attacker.Endpoints {
+			path := Config.Server + endpoint
+			targets = append(targets, vegeta.Target{
+				Method: "GET",
+				URL:    path,
+			})
+		}
+
+		targeter := vegeta.NewStaticTargeter(targets...)
+		// Truncate all response bodies at 100 bytes
+		vegAttacker := vegeta.NewAttacker(vegeta.MaxBody(100))
+
+		wg.Add(1)
+		go func(attacker *Attacker) {
+			defer wg.Done()
+			remainder := duration
+			if attacker.Delay > 0 {
+				if attacker.Delay >= float32(attackProfile.Duration) {
+					log.Printf("The specified delay, %f, for attacker %d is >= the duration of test, %d.", attacker.Delay, idx, Config.Attack)
+					return
+				}
+				attackDelay := time.Duration(int(attacker.Delay*1000)) * time.Millisecond
+				time.Sleep(attackDelay)
+				remainder -= attackDelay
+			}
+			if attacker.Name != "" {
+				log.Printf("Beggining %s", attacker.Name)
+			}
+			for res := range vegAttacker.Attack(targeter, rate, remainder, fmt.Sprintf("%s:%d", Config.Attack, idx)) {
+				resChan <- res
+			}
+		}(attacker)
+	}
+
+	// Make a notification channel for the end of the attack.
+	c := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+
+	// A couple of helper functions.
+	// Vegeta doesn't return the url in the result, so look it up,
+	// finding the Attacker along the way.
+	findAttacker := func(result *vegeta.Result) (attacker *Attacker, endpoint string) {
+		parts := strings.Split(result.Attack, ":")
+		attackerIdx, err := strconv.Atoi(parts[len(parts)-1])
+		if err != nil {
+			log.Fatalf("Failed to parse attacker index. Something is terribly wrong.")
+			return
+		}
+		attacker = attackProfile.Attackers[attackerIdx]
+		endpoint = attacker.Endpoints[int(result.Seq)%len(attacker.Endpoints)]
+		return
+	}
+
+	// A list of unique error URLs. Print messages for new ones. Ignore repeats.
+	var problematic []string
+	addProblematic := func(result *vegeta.Result) {
+		_, endpoint := findAttacker(result)
+		for _, url := range problematic {
+			if url == endpoint {
+				return
+			}
+		}
+		problematic = append(problematic, endpoint)
+		log.Printf("Error code %d from %s: %s",
+			result.Code, endpoint, string(result.Body))
+	}
+
+	delay := time.Duration(1) * time.Millisecond
+	var metrics vegeta.Metrics
+	var res *vegeta.Result
+
+	// Monitor results. Wait for end-of-attack signal.
+out:
+	for {
+		select {
+		case res = <-resChan:
+			if res.Code != 200 {
+				addProblematic(res)
+			}
+			// If more info is needed for e.g. plotting, grab it here.
+			metrics.Add(res)
+		case <-c:
+			break out
+		case <-time.After(delay):
+		}
+	}
+	metrics.Close()
+
+	// Vegeta report to terminal.
+	vegeta.NewTextReporter(&metrics)(os.Stdout)
+
+	// JSON results file.
+	fmtTime := time.Now().Format("2016-01-02-15-04-05")
+	var jsonBytes []byte
+	if Config.FormatResults {
+		jsonBytes, err = json.MarshalIndent(metrics, "", "    ")
+	} else {
+		jsonBytes, err = json.Marshal(metrics)
+	}
+	if err != nil {
+		log.Fatalf("Failed to parse json from vegeta.Metrics for %s:", Config.Attack)
+	}
+	path := filepath.Join(Config.ResultDirectory, fmt.Sprintf(OutputFileTemplate, Config.Attack, fmtTime))
+	err = ioutil.WriteFile(path, jsonBytes, 0755)
+	if err != nil {
+		log.Fatalf("Failed to write result file to %s", path)
+	}
+
+	return
+}
+
+func createResultsDirectory() (err error) {
+	fileInfo, err := os.Stat(Config.ResultDirectory)
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(DefaultResultDirectory, 0755)
+		if err != nil {
+			return fmt.Errorf("Unable to create results directory: %v", err)
+		}
+	} else if err != nil {
+		return fmt.Errorf("Unable to retrieve file system information: %v", err)
+	} else if !fileInfo.IsDir() {
+		return fmt.Errorf("Unable to create results directory due to a filename conflict.")
+	}
+	return
+}
+
+func loadProfiles() (profiles map[string]*Attack, err error) {
+	jsonBytes, err := ioutil.ReadFile(Config.ProfilesPath)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to load attack profile definitions from %s: %v", Config.ProfilesPath, err)
+	}
+
+	if err = json.Unmarshal(jsonBytes, &profiles); err != nil {
+		return nil, fmt.Errorf("Failed to parse json from attack profiles: %v", err)
+	}
+
+	if len(profiles) == 0 {
+		return nil, fmt.Errorf("No attack profiles found.")
+	}
+
+	return
+}
+
+func listAttacks(profiles map[string]*Attack) {
+	fmt.Println("available attacks")
+	fmt.Println("-----------------")
+	for key, _ := range profiles {
+		fmt.Println(key)
+	}
+}
