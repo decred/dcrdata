1505b49e2135c9108d639f4b2bb2fcf3c9444066
Jonathan Chappelow <chappjc@protonmail.com>
2019-03-15 08:53:09 -0500

    txhelpers: store previously-computed ultimate subsidy values

--- txhelpers/subsidy.go
+++ txhelpers/subsidy.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2018 The Decred developers
+// Copyright (c) 2015-2019 The Decred developers
 // Copyright (c) 2013-2015 The btcsuite developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
@@ -10,12 +10,21 @@ import (
 	"github.com/decred/dcrd/chaincfg"
 )
 
+// ultimateSubsidies stores ultimate subsidy values computed by UltimateSubsidy.
+var ultimateSubsidies = map[*chaincfg.Params]int64{}
+
 // UltimateSubsidy computes the total subsidy over the entire subsidy
 // distribution period of the network.
 func UltimateSubsidy(params *chaincfg.Params) int64 {
+	// Check previously computed ultimate subsidies.
+	totalSubsidy, ok := ultimateSubsidies[params]
+	if ok {
+		return totalSubsidy
+	}
+
 	subsidyCache := blockchain.NewSubsidyCache(0, params)
 
-	totalSubsidy := params.BlockOneSubsidy()
+	totalSubsidy = params.BlockOneSubsidy()
 	for i := int64(0); ; i++ {
 		// Genesis block or first block.
 		if i <= 1 {
@@ -49,6 +58,10 @@ func UltimateSubsidy(params *chaincfg.Params) int64 {
 			}
 		}
 	}
+
+	// Update the ultimate subsidy store.
+	ultimateSubsidies[params] = totalSubsidy
+
 	return totalSubsidy
 }
 
--- txhelpers/subsidy_test.go
+++ txhelpers/subsidy_test.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2018-2019 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
 package txhelpers
 
 import (
@@ -6,10 +10,63 @@ import (
 	"github.com/decred/dcrd/chaincfg"
 )
 
-func TestBlockSubsidy(t *testing.T) {
+func TestUltimateSubsidy(t *testing.T) {
+	// Mainnet
+	wantMainnetSubsidy := int64(2099999999800912)
 	totalSubsidy := UltimateSubsidy(&chaincfg.MainNetParams)
 
-	if totalSubsidy != 2099999999800912 {
-		t.Errorf("Bad total subsidy; want 2099999999800912, got %v", totalSubsidy)
+	if totalSubsidy != wantMainnetSubsidy {
+		t.Errorf("Bad total subsidy; want %d, got %d",
+			wantMainnetSubsidy, totalSubsidy)
+	}
+
+	// verify cache
+	totalSubsidy2 := UltimateSubsidy(&chaincfg.MainNetParams)
+	if totalSubsidy != totalSubsidy2 {
+		t.Errorf("Bad total subsidy; want %d, got %d",
+			totalSubsidy, totalSubsidy2)
+	}
+
+	// Testnet
+	wantTestnetSubsidy := int64(526540305161472)
+	totalTNSubsidy := UltimateSubsidy(&chaincfg.TestNet3Params)
+
+	if totalTNSubsidy != wantTestnetSubsidy {
+		t.Errorf("Bad total subsidy; want %d, got %d",
+			wantTestnetSubsidy, totalTNSubsidy)
+	}
+
+	// verify cache
+	totalTNSubsidy2 := UltimateSubsidy(&chaincfg.TestNet3Params)
+	if totalTNSubsidy != totalTNSubsidy2 {
+		t.Errorf("Bad total subsidy; want %d, got %d",
+			totalTNSubsidy, totalTNSubsidy2)
+	}
+
+	// re-verify mainnet cache
+	totalSubsidy3 := UltimateSubsidy(&chaincfg.MainNetParams)
+	if totalSubsidy != totalSubsidy3 {
+		t.Errorf("Bad total subsidy; want %d, got %d",
+			totalSubsidy, totalSubsidy3)
+	}
+}
+
+func BenchmarkUltimateSubsidy(b *testing.B) {
+	// warm up
+	totalSubsidy := UltimateSubsidy(&chaincfg.MainNetParams)
+	// verify cache
+	totalSubsidy2 := UltimateSubsidy(&chaincfg.MainNetParams)
+	if totalSubsidy != totalSubsidy2 {
+		b.Errorf("Bad total subsidy; want %d, got %d",
+			totalSubsidy, totalSubsidy2)
+	}
+
+	for i := 0; i < b.N; i++ {
+		totalSubsidy = UltimateSubsidy(&chaincfg.MainNetParams)
+	}
+
+	if totalSubsidy != totalSubsidy2 {
+		b.Errorf("Bad total subsidy; want %d, got %d",
+			totalSubsidy, totalSubsidy2)
 	}
 }
