bcdc149a057ce8cfcb70a46dd22ef58a0253dfc8
Jonathan Chappelow <chappjc@protonmail.com>
2019-03-26 09:59:20 -0500

    blockdata module
    dcrdata imports and mod

--- api/insight/socket.io.go
+++ api/insight/socket.io.go
@@ -12,8 +12,8 @@ import (
 	"github.com/decred/dcrd/dcrjson/v2"
 	"github.com/decred/dcrd/dcrutil"
 	"github.com/decred/dcrd/wire"
+	"github.com/decred/dcrdata/blockdata"
 	"github.com/decred/dcrdata/txhelpers"
-	"github.com/decred/dcrdata/v4/blockdata"
 	socketio "github.com/googollee/go-socket.io"
 )
 
