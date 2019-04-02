e4606cd2664797a473c29236c3acf219ad09ec62
Jonathan Chappelow <chappjc@protonmail.com>
2019-04-02 08:32:57 -0500

    bump dcrdata version to 4.0.2

--- version/version.go
+++ version/version.go
@@ -26,10 +26,10 @@ const (
 	AppName  string = "dcrdata"
 	AppMajor uint   = 4
 	AppMinor uint   = 0
-	AppPatch uint   = 0
+	AppPatch uint   = 2
 )
 
-// go build -ldflags "-X github.com/decred/dcrdata/v4/version.appPreRelease= -X github.com/decred/dcrdata/v4/version.appBuild=`git rev-parse --short HEAD`"
+// go build -v -o dcrdata -ldflags "-X github.com/decred/dcrdata/v4/version.appPreRelease= -X github.com/decred/dcrdata/v4/version.appBuild=`git rev-parse --short HEAD`"
 var (
 	// appPreRelease is defined as a variable so it can be overridden during the
 	// build process. It MUST only contain characters from semanticAlphabet per
