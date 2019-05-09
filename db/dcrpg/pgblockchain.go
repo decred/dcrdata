5e72abab448928a1d96c98ce5fd7bf59e1aa76f7
Brian <buck54321@gmail.com>
2019-05-09 09:44:43 -0500

    restart handler. print stack trace

--- db/dcrpg/pgblockchain.go
+++ db/dcrpg/pgblockchain.go
@@ -13,6 +13,7 @@ import (
 	"fmt"
 	"reflect"
 	"runtime"
+	"runtime/debug"
 	"strconv"
 	"strings"
 	"sync"
@@ -1113,13 +1114,6 @@ func (pgb *ChainDB) VotesInBlock(hash string) (int16, error) {
 	return voters, nil
 }
 
-// piparser panics should not cause dcrdata panics.
-func recoverPiparser() {
-	if r := recover(); r != nil {
-		log.Errorf("recovered from piparser panic: %v", r)
-	}
-}
-
 // proposalsUpdateHandler runs in the background asynchronous to retrieve the
 // politeia proposal updates that the piparser tool signaled.
 func (pgb *ChainDB) proposalsUpdateHandler() {
@@ -1130,7 +1124,18 @@ func (pgb *ChainDB) proposalsUpdateHandler() {
 	}
 
 	go func() {
-		defer recoverPiparser()
+		defer func() {
+			if r := recover(); r != nil {
+				log.Errorf("recovered from piparser panic in proposalsUpdateHandler: %v", r)
+				log.Errorf(string(debug.Stack()))
+				select {
+				case <-time.NewTimer(time.Minute).C:
+					log.Infof("attempting to restart proposalsUpdateHandler")
+					pgb.proposalsUpdateHandler()
+				case <-pgb.ctx.Done():
+				}
+			}
+		}()
 		for range pgb.piparser.UpdateSignal() {
 			count, err := pgb.PiProposalsHistory()
 			if err != nil {
