da5b141457d4a637c9f7bd49e3dbb683b4d58aa1
Jonathan Chappelow <chappjc@users.noreply.github.com>
2019-06-05 15:58:16 +0000

    update node deps
    * update node deps
    
    * update stylelint to 10.0.1
    
    * update clean-webpack-plugin to 3.0.0
    
    * upgrade to css-loader 2.1.1
    
    * upgrade eslint-plugin-node to 9.1.0 with node.js 12 support
    
    * update axios to 0.19.0. Consider using new getUri method.
    
    * update mini-css-extract-plugin. Consider hot module reloading (HMR).

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const StyleLintPlugin = require('stylelint-webpack-plugin')
 
@@ -24,7 +24,7 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              url: false, sourceMap: true
+              modules: false, url: false, sourceMap: true
             }
           },
           {
@@ -38,7 +38,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
