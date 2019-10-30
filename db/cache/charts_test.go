2d550ccbb2cc3ba4a089c5311f59895a2e502d41
Jonathan Chappelow <chappjc@protonmail.com>
2019-10-29 20:57:29 -0500

    db/cache v3.0.0

--- db/cache/charts_test.go
+++ db/cache/charts_test.go
@@ -14,7 +14,7 @@ import (
 
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/chaincfg/v2"
-	"github.com/decred/dcrdata/txhelpers/v3"
+	"github.com/decred/dcrdata/txhelpers/v4"
 )
 
 var tempDir string
