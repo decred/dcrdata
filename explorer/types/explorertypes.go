58a53e1525fb0d6a15daf09f8f708585f66b24fc
Jonathan Chappelow <chappjc@protonmail.com>
2019-11-11 16:06:21 -0600

    views: tag mix tx on tx page
    include fee in ticket price in IsMixedSplitTx
    
    steal txsizes from dcrwallet/internal/txsizes
    
    return mixDenom from IsMixedSplitTx
    
    txWithTicketPrice: really ugly way to get txraw and price for same best block

--- explorer/types/explorertypes.go
+++ explorer/types/explorertypes.go
@@ -170,6 +170,9 @@ type TrimmedTxInfo struct {
 // TxInfo models data needed for display on the tx page
 type TxInfo struct {
 	*TxBasic
+	IsMix            bool
+	MixDenom         int64
+	MixCount         uint32
 	SpendingTxns     []TxInID
 	Type             string
 	Vin              []Vin
