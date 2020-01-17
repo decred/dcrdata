af8fa7e24b18498fc3605893cc2f9cd548d332cf
Jonathan Chappelow <chappjc@protonmail.com>
2020-01-17 14:31:28 -0600

    v5.2.2 patch bump

--- version/version.go
+++ version/version.go
@@ -26,7 +26,7 @@ const (
 	AppName  string = "dcrdata"
 	AppMajor uint   = 5
 	AppMinor uint   = 2
-	AppPatch uint   = 1
+	AppPatch uint   = 2
 )
 
 // go build -v -ldflags "-X github.com/decred/dcrdata/v5/version.appPreRelease= -X github.com/decred/dcrdata/v5/version.appBuild=`git rev-parse --short HEAD`"
