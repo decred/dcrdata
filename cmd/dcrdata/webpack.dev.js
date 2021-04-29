c215a13211b295ac41d9dfd6a8c492b44a3b1fe4
Jonathan Chappelow <chappjc@protonmail.com>
2021-04-29 13:12:59 -0500

    remove webpack-dev-server

--- cmd/dcrdata/webpack.dev.js
+++ cmd/dcrdata/webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
-const path = require('path')
 const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = merge(common, {
@@ -8,10 +7,5 @@ module.exports = merge(common, {
   plugins: [new ESLintPlugin({
     formatter: 'stylish'
   })],
-  devtool: 'inline-source-map',
-  devServer: {
-    static: path.resolve(__dirname, 'public/index.js'), // './public/index.js',
-    port: 7777,
-    watch: true
-  }
+  devtool: 'inline-source-map'
 })
