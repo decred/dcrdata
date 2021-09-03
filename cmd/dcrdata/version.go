d434a311d9c1ee1e326c780273d0528e31ac2073
Jonathan Chappelow <chappjc@protonmail.com>
2021-09-03 15:04:31 -0500

    release.local for stable branch

--- cmd/dcrdata/version.go
+++ cmd/dcrdata/version.go
@@ -29,17 +29,17 @@ const (
 	AppPatch uint   = 0
 )
 
-// go build -v -ldflags "-X github.com/decred/dcrdata/v5/version.appPreRelease= -X github.com/decred/dcrdata/v5/version.appBuild=`git rev-parse --short HEAD`"
+// go build -v -ldflags "-X github.com/decred/dcrdata/v6/version.appPreRelease= -X github.com/decred/dcrdata/v6/version.appBuild=`git rev-parse --short HEAD`"
 var (
 	// appPreRelease is defined as a variable so it can be overridden during the
 	// build process. It MUST only contain characters from semanticAlphabet per
 	// the semantic versioning spec.
-	appPreRelease = "pre"
+	appPreRelease = ""
 
 	// appBuild is defined as a variable so it can be overridden during the
 	// build process. It MUST only contain characters from semanticBuildAlphabet
 	// per the semantic versioning spec.
-	appBuild = "dev"
+	appBuild = "release.local"
 )
 
 // Version returns the application version as a properly formed string per the
