5a691731346f967beebae5dcf8b8025df5acccd9
Jonathan Chappelow <chappjc@protonmail.com>
2021-09-17 19:42:10 -0500

    restore desktop notification functionality

--- cmd/dcrdata/public/js/services/desktop_notification_service.js
+++ cmd/dcrdata/public/js/services/desktop_notification_service.js
@@ -1,20 +1,6 @@
-import globalEventBus from './event_bus_service'
 import Notify from 'notifyjs'
 
-function onShowNotification () {
-  console.log('block ntfn shown')
-}
-function onCloseNotification () {
-  console.log('block ntfn closed')
-}
-function onClickNotification () {
-  console.log('block ntfn clicked')
-}
-function onErrorNotification () {
-  console.error('Error showing notification. You may need to request permission.')
-}
-
-function notifyNewBlock (newBlock) {
+export function notifyNewBlock (newBlock) {
   if (Notify.needsPermission) return
   const block = newBlock.block
   const newBlockNtfn = new Notify('New Decred Block Mined', {
@@ -22,13 +8,8 @@ function notifyNewBlock (newBlock) {
     tag: 'blockheight',
     image: '/images/dcrdata144x128.png',
     icon: '/images/dcrdata144x128.png',
-    notifyShow: onShowNotification,
-    notifyClose: onCloseNotification,
-    notifyClick: onClickNotification,
-    notifyError: onErrorNotification,
-    timeout: 10
+    notifyError: (e) => console.error('Error showing notification:', e),
+    timeout: 7
   })
   newBlockNtfn.show()
 }
-
-globalEventBus.on('BLOCK_RECEIVED', notifyNewBlock)
