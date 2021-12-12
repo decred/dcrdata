d17f4e7f8bc5832c6ce1e075d3b19d7e7f0f511d
Jonathan Chappelow <chappjc@protonmail.com>
2021-12-12 11:10:41 -0600

    CI: update lint.sh and run_tests.sh with go mod tidy
    Have each script do the go mod tidy check.
    Also shellcheck and fix them both.

--- lint.sh
+++ lint.sh
@@ -7,14 +7,13 @@
 #
 #   lint.sh [folder] - lints all modules in the specified folder.
 
-REPO=`git rev-parse --show-toplevel 2> /dev/null`
-if [[ $? != 0 ]]; then
-    REPO=`pwd`
+if ! REPO=$(git rev-parse --show-toplevel 2> /dev/null); then
+    REPO=$(pwd)
 fi
 
 # Root is the input argument, or REPO if no input argument.
 ROOT=${1:-$REPO}
-echo "Linting all modules under" $ROOT
+echo "Linting all modules under ${ROOT}"
 
 set -e
 
@@ -22,13 +21,19 @@ shopt -s expand_aliases
 
 export GO111MODULE=on
 
-go version
+GV=$(go version | sed "s/^.*go\([0-9.]*\).*/\1/")
+echo "Go version: $GV"
 
-pushd $ROOT > /dev/null
+pushd "$ROOT" > /dev/null
 
-MODPATHS=$(find . -name go.mod -type f -print)
+# Do the module paths in order so that go mod tidy updates will cascade to
+# dependent modules.
+MODPATHS="./go.mod ./exchanges/go.mod ./gov/go.mod ./db/dcrpg/go.mod ./cmd/dcrdata/go.mod \
+    ./pubsub/democlient/go.mod ./cmd/swapscan-btc/go.mod ./testutil/dbload/go.mod \
+    ./testutil/apiload/go.mod ./exchanges/rateserver/go.mod"
+#MODPATHS=$(find . -name go.mod -type f -print)
 
-alias superlint='golangci-lint run --deadline=10m \
+alias superlint="golangci-lint run --deadline=10m \
     --disable-all \
     --enable govet \
     --enable staticcheck \
@@ -40,18 +45,28 @@ alias superlint='golangci-lint run --deadline=10m \
     --enable misspell \
     --enable unparam \
     --enable asciicheck \
-    --enable makezero'
+    --enable makezero"
 
 # run lint on all listed modules
 set +e
 ERROR=0
 trap 'ERROR=$?' ERR
 for MODPATH in $MODPATHS; do
-    module=$(dirname ${MODPATH})
-    pushd $module > /dev/null
+    module=$(dirname "${MODPATH}")
+    pushd "$module" > /dev/null
     MOD=$(go list -m)
-    echo "Linting:" $MOD
+    echo "Linting: $MOD"
     superlint
+    if [[ "$GV" =~ ^1.17 ]]; then
+		MOD_STATUS=$(git status --porcelain go.mod go.sum)
+		go mod tidy
+		UPDATED_MOD_STATUS=$(git status --porcelain go.mod go.sum)
+		if [ "$UPDATED_MOD_STATUS" != "$MOD_STATUS" ]; then
+			echo "$module: running 'go mod tidy' modified go.mod and/or go.sum"
+		git diff --unified=0 go.mod go.sum
+			exit 1
+		fi
+	fi
     popd > /dev/null
 done
 
