fab8a7e7bde6f4382ae9613db706a41788e7a0f3
Thiago F. Figueiredo <thiagofigueiredo777@gmail.com>
2022-02-01 20:40:44 -0300

    webpack: Add plugin to replace bundle hashes
    This diff adds automatic cache bust by adding an inline webpack
    plugin, which parses the bundles hashed filenames through the app
    entrypoints and replaces them on the extras.tmpl template.

--- cmd/dcrdata/webpack.common.js
+++ cmd/dcrdata/webpack.common.js
@@ -2,6 +2,8 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const StyleLintPlugin = require('stylelint-webpack-plugin')
+const Path = require("path");
+const FileSystem = require("fs");
 
 module.exports = {
   entry: {
@@ -47,16 +49,43 @@ module.exports = {
       // both options are optional
       // filename: '[name].css',
       // chunkFilename: '[id].css'
-      filename: 'css/style.css'
+      filename: 'css/style.[contenthash].css'
     }),
     new StyleLintPlugin({
       threads: true,
       allowEmptyInput: true, // avoid errors with .stylelintignore
-    })
+    }),
+    new class ReplaceBuildHash {
+      apply(compiler) {
+        compiler.hooks.done.tap('ReplaceBuildHash', (stats) => {
+          const s = stats.toJson()
+          if (s.errors.length) {
+            // Do nothing on error
+            return;
+          }
+          // Get filenames with hashes from app assets
+          const hashes = s.entrypoints.app.assets.reduce((obj, v) => {
+            if (v.name.includes('js/4')) obj.vendor = v.name.split('/')[1]
+            if (v.name.includes('css/style')) obj.css = v.name.split('/')[1]
+            if (v.name.includes('js/app')) obj.app = v.name.split('/')[1]
+            return obj
+          }, {})
+          // Update js/css bundles with the generated cache hash on the
+          // extras.tmpl file
+          const tmplPath = Path.join(__dirname, './views/extras.tmpl')
+          const tmplIn = FileSystem.readFileSync(tmplPath, "utf8")
+          const tmplOut = tmplIn
+            .replace(new RegExp('4.(\\w)\\w+\\.bundle\\.js'), `${hashes.vendor}`)
+            .replace(new RegExp('style.(\\w)\\w+\\.css'), `${hashes.css}`)
+            .replace(new RegExp('app.(\\w)\\w+\\.bundle\\.js'), `${hashes.app}`)
+          FileSystem.writeFileSync(tmplPath, tmplOut)
+        });
+      }
+    }
   ],
   output: {
     hashFunction: "xxhash64",
-    filename: 'js/[name].bundle.js',
+    filename: 'js/[name].[contenthash].bundle.js',
     path: path.resolve(__dirname, 'public/dist'),
     publicPath: '/dist/'
   },
