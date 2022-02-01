ec540533b7cbc0163bc9717bd3e67505551bbe77
Thiago Figueiredo <thiagofigueiredo777@gmail.com>
2022-01-31 21:20:50 -0300

    webpack: Add plugin to replace bundle hashes

--- cmd/dcrdata/webpack.common.js
+++ cmd/dcrdata/webpack.common.js
@@ -2,6 +2,8 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const StyleLintPlugin = require('stylelint-webpack-plugin')
+const Path = require("path");
+const FileSystem = require("fs");
 
 module.exports = {
   entry: {
@@ -47,16 +49,37 @@ module.exports = {
       // both options are optional
       // filename: '[name].css',
       // chunkFilename: '[id].css'
-      filename: 'css/style.css'
+      filename: 'css/style.[fullhash].css'
     }),
     new StyleLintPlugin({
       threads: true,
       allowEmptyInput: true, // avoid errors with .stylelintignore
-    })
+    }),
+    new class ReplaceBuildHash {
+      apply(compiler) {
+        compiler.hooks.done.tap('ReplaceBuildHash', (stats) => {
+          const s = stats.toJson()
+          if (s.errors.length) {
+            // Do nothing on error
+            return;
+          }
+          // Update js/css bundles with the generated cache hash on the
+          // extras.tmpl file
+          const jsRegexp = new RegExp('(\\w)\\w+\\.bundle\\.js', 'g')
+          const cssRegexp = new RegExp('(\\w)\\w+\\.css', 'g')
+          const tmplPath = Path.join(__dirname, './views/extras.tmpl')
+          const tmplIn = FileSystem.readFileSync(tmplPath, "utf8")
+          const tmplOut = tmplIn
+            .replace(jsRegexp, `${s.hash}.bundle.js`)
+            .replace(cssRegexp, `${s.hash}.css`)
+          FileSystem.writeFileSync(tmplPath, tmplOut)
+        });
+      }
+    }
   ],
   output: {
     hashFunction: "xxhash64",
-    filename: 'js/[name].bundle.js',
+    filename: 'js/[name].[fullhash].bundle.js',
     path: path.resolve(__dirname, 'public/dist'),
     publicPath: '/dist/'
   },
